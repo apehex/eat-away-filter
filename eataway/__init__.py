8a0437667a7b53e64ed97545757cfe189db59d8a
apehex <apehex@protonmail.com>
2021-01-31 16:29:16 +0100

    Rename the module

---
+++ eataway/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+===============
+Eat Away Filter
+===============
+
+An image filter that slowly eats the content away.
+"""
+
+from __future__ import division, print_function, absolute_import
+from importlib.metadata import version
+
+__author__ = 'apehex'
+__email__ = 'apehex@protonmail.com'
+__version__ = version(__package__.split('.')[0])
+__title__ = 'eataway'
+__description__ = 'An image filter that slowly eats the content away'
+__url__ = 'https://github/apehex/eataway'
+__license__ = 'MIT license'
+
+__all__ = []
