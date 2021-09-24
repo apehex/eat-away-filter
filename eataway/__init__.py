0d63e694fc4803694e2d15cfc4575e6b7f9fd0fe
apehex <apehex@protonmail.com>
2021-09-24 12:33:06 +0200

    Remove the poetry lock

--- eataway/__init__.py
+++ eataway/__init__.py
@@ -15,12 +15,12 @@ from eataway.filter import (
     eat_image_away,
     merge_into_an_animation)
 
-__author__ = 'apehex'
+__author__ = 'Apehex'
 __email__ = 'apehex@protonmail.com'
 __version__ = version(__package__.split('.')[0])
 __title__ = 'eataway'
 __description__ = 'An image filter that slowly eats the content away'
-__url__ = 'https://github/apehex/eataway'
+__url__ = 'https://github/apehex/eat-away-filter'
 __license__ = 'MIT license'
 
 __all__ = [
